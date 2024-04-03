48c9e730f3498f04556d5d36b88cd3b833c4118c
Jonthan JG <jonysthil@gmail.com>
2024-04-02 18:15:03 -0600

    ✨ FEAT: Cambio de logos

--- form.php
+++ form.php
@@ -2,5 +2,5 @@
 $a = $_POST['whats'];
 $b = $_POST['contact'];
 $c = $_POST['country'];
-header("Location:https://api.whatsapp.com/send?phone=$c$a&text=Hola%20$b%20te%20comparto%20mi%20VCard:%20https://vcard.jonyjg.com/");
+header("Location:https://wa.me/send?phone=$c$a&text=Hola%20$b%20te%20comparto%20mi%20VCard:%20https://vcard.jonyjg.com/");
 ?>
